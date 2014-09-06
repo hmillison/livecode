9ee40ffb2cde5ecb5f6846125b811ed5f74fac09
Timotius Sitorus <timsitorus1@gmail.com>
2014-09-06 17:40:01 -0400

    added session saves

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.get('/chat', function(req, res) {
 	var roomID;
 
 	if(req.query.create === 'true'){
+		room.set('content', '//Write Your Code Here');
 		room.save(null, {
 		  success: function(room) {
 		    
@@ -28,14 +29,25 @@ router.get('/chat', function(req, res) {
 
 			nsp.on('connection', function(socket){
 			  	console.log('someone connected');
+			  	var query = new Parse.Query(Rooms);
 
 			  	socket.on('editorChange', function (data) {
+				  	query.get(room.id, {
+						success: function(room) {
+						  	room.set("content", data.newValue);
+						  	room.save();
+						},
+					    error: function(object, error) {
+					    	console.log('Failed to load room: '+ error.message);
+					    }
+					});
+
 	        		nsp.emit('editorCallback', data);
 	        	});
 
 			});
 
-		    res.render('chat', { title: 'LiveCode', id:room.id });
+		    res.render('chat', { title: 'LiveCode', id:room.id , content:room.get('content')});
 
 		  },
 		  error: function(room, error) {
@@ -48,14 +60,24 @@ router.get('/chat', function(req, res) {
 		var query = new Parse.Query(Rooms);
 		query.get(req.query.id, {
 		  	success: function(room) {
-			  	res.render('chat', { title: 'LiveCode', id:req.query.id });
-
+			  	res.render('chat', { title: 'LiveCode', id:req.query.id, content: room.get('content') });
 				var nsp = io.of('/'+ req.query.id);
 
 				nsp.on('connection', function(socket){
 				  	console.log('someone connected');
+				  	var query = new Parse.Query(Rooms);
 
 				    socket.on('editorChange', function (data) {
+				    	query.get(room.id, {
+							success: function(room) {
+							  	room.set("content", data.newValue);
+							  	room.save();
+							},
+						    error: function(object, error) {
+						    	console.log('Failed to load room: '+ error.message);
+						    }
+						});
+
 		        		nsp.emit('editorCallback', data);
 		        	});
 				});
