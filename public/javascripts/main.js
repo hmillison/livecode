2935033d52ac00c780d246c2aa3c1f2a31911dea
Timotius Sitorus <timsitorus1@gmail.com>
2014-09-06 04:13:17 -0400

    adding stuff

---
+++ public/javascripts/main.js
@@ -0,0 +1,32 @@
+var webrtc = new SimpleWebRTC({
+    localVideoEl: 'localVideo',
+    remoteVideosEl: 'remotesVideos',
+    autoRequestMedia: true
+});
+
+webrtc.on('readyToCall', function () {
+    webrtc.joinRoom('<%= id %>');
+});
+
+var socket = io('/<%= id %>');
+var changing = false;
+
+socket.on('editorCallback', function(data){
+    changing = true;
+    editor.setValue(data.newValue);
+    editor.clearSelection()
+    changing = false;
+});
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/javascript");
+
+editor.getSession().on('change', function(e){
+    if(!changing){
+        var data = {    
+            newValue: editor.getValue()
+        }
+        socket.emit('editorChange', data);
+    }
+});
